00c770e2f636fd3e07653e6a9328343f76708c0d
joassaff <jassaff@itba.edu.ar>
2022-03-16 16:46:55 -0300

    prints bppv

--- TP1/algoritmos/bppv.py
+++ TP1/algoritmos/bppv.py
@@ -2,7 +2,7 @@ from TP1.utiles.nodos import Nodo
 import time
 
 LIMITE_DEFAULT = 20
-LIMITE_AUMENTO = 5
+LIMITE_AUMENTO = 1
 
 
 def bppv(inicio, objetivo, limite):
@@ -13,11 +13,16 @@ def bppv(inicio, objetivo, limite):
 
     fondo_arbol = False
 
+    expandidos = []
+    frontera = []
+
     while not fondo_arbol:
-        resultado, fondo_arbol = bppv_rec(Nodo(inicio, None, None, 0), objetivo, 0, limite)
+        resultado, fondo_arbol = bppv_rec(Nodo(inicio, None, None, 0), objetivo, 0, limite, expandidos, frontera)
         if resultado is not None:
             end_time = time.process_time()
             print("Llegó al objetivo!")
+            print("Cantidad de nodos expandidos: ", len(expandidos))
+            print("Cantidad de nodos frontera: ", len(frontera))
             print("Profundidad: ", resultado.profundidad)
             print('Costo: ', resultado.profundidad)
             print('Limite final: ', limite)
@@ -31,7 +36,10 @@ def bppv(inicio, objetivo, limite):
     print('Tiempo de búsqueda: ', end_time - start_time)
 
 
-def bppv_rec(nodo, objetivo, profundidad, limite):
+def bppv_rec(nodo, objetivo, profundidad, limite, expandidos, frontera):
+    expandidos.append(nodo)
+    if nodo in frontera:
+        frontera.remove(nodo)
     if nodo.estado == objetivo:
         return nodo, True
 
@@ -42,8 +50,9 @@ def bppv_rec(nodo, objetivo, profundidad, limite):
             return None, True
 
     fondo_arbol = True
+    frontera.extend(nodo.encontrar_sucesores())
     for i in range(len(nodo.encontrar_sucesores())):
-        resultado, fondo_arbol_rec = bppv_rec(nodo.encontrar_sucesores()[i], objetivo, profundidad + 1, limite)
+        resultado, fondo_arbol_rec = bppv_rec(nodo.encontrar_sucesores()[i], objetivo, profundidad + 1, limite, expandidos, frontera)
 
         if resultado is not None:
             return resultado, True

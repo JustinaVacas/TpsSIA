4052a25bde699b3f76d0625b6b9f8caf275a9a54
Ian Mejalelaty <imejalelaty@itba.edu.ar>
2022-04-04 20:43:18 -0300

    ordering files

--- TP2/util/selection.py
+++ TP2/util/selection.py
@@ -2,8 +2,8 @@ import math
 import random
 import numpy
 
-from TP2.function import error
-from TP2.population_0 import Individual
+from TP2.util.function import error
+from TP2.populations.population_0 import Individual
 
 
 def sort_population_by_fitness(individual):

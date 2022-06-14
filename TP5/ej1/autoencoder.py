fa56fbca8d8a8e33caf1e58766044b600e0fb492
justinavacas <vacaslocas123>
2022-06-14 15:57:46 -0300

    comienzo tp5

---
+++ TP5/ej1/autoencoder.py
@@ -0,0 +1,12 @@
+from TP5.ej1.util.multiple_perceptron import MultilayerPerceptron
+# from fonts import get_input, get_output
+
+eta = 0.1
+momentum = 0.9
+hidden_layers = [35, 5, 35]
+epochs = 1000
+beta = 0.7
+
+# mp1 = MultilayerPerceptron(inputs, outputs, eta, hidden_layers, adaptive_eta_params, beta, batch, momentum)
+
+# mp1.train(epochs)
